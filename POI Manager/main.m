7b268261e852f50ccdb7950270a5ef80fd6fe20f
carlos calderon <carlosdevelopment1979@gmail.com>
2016-04-12 19:17:20 -0500

    Initial Commit

---
+++ POI Manager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  POI Manager
+//
+//  Created by carlos calderon on 4/12/16.
+//  Copyright © 2016 carlos calderon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
