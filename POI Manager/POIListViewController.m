7effacc28e870d2807462f6656755e44d5059a0c
carlos calderon <carlosdevelopment1979@gmail.com>
2016-04-14 01:40:51 -0500

    Configure And Customize POIFourSquareSessonManager

--- POI Manager/POIListViewController.m
+++ POI Manager/POIListViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "POIListViewController.h"
+#import "POIFourSquareSessionManager.h"
+#import <MagicalRecord/MagicalRecord.h>
+#import "AFMMRecordResponseSerializationMapper.h"
+#import "AFMMRecordResponseSerializer.h"
+
 //Client id
 //RODUBYAI1CMKTSLDI04NLRU53HXLKSWR43GYICGD3CUPP0FN
 //Client secret
@@ -24,6 +29,20 @@ static NSString * const kCLIENTSECRET = @"VG5P3M0XMPZ1ZHMW3UTUJHM2R3QPSU4FATDKCK
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    POIFourSquareSessionManager *sessionManager = [POIFourSquareSessionManager sharedClient];
+    
+    NSManagedObjectContext *context = [NSManagedObjectContext MR_defaultContext];
+    
+    AFHTTPResponseSerializer *HTTPResponseSerializer = [AFJSONResponseSerializer serializer];
+    
+    AFMMRecordResponseSerializationMapper *mapper = [[AFMMRecordResponseSerializationMapper alloc] init];
+    
+    [mapper registerEntityName:@"Venue" forEndpointPathComponent:@"venues/search?"];
+    
+    AFMMRecordResponseSerializer *serializer = [AFMMRecordResponseSerializer serializerWithManagedObjectContext:context responseObjectSerializer:HTTPResponseSerializer entityMapper:mapper];
+    
+    sessionManager.responseSerializer = serializer;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
